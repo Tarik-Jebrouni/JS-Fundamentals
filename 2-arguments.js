ea8d1081cada12314ecb4496574ecf9e0348b8c1
Tarik Jebrouni <tarik.hani74@gmail.com>
2025-05-20 21:36:13 +0100

    Arguments

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+// Check the number of command-line arguments 
+const args = "Tarik JB";
+
+if (args.length === 0) {
+  console.log("No argument");
+} else if (args.length === 1) {
+  console.log("Argument found",args);
+} else {
+  console.log("Arguments found",args);
+}
